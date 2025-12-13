3f1aa9bc29fbc2245125eee7ea4fe96c87d36d9d
Prasad Shedage <prasadshedage1998@gmail.com>
2025-12-13 13:49:19 +0530

    new changes

--- DecendingOrderComparator.java
+++ DecendingOrderComparator.java
@@ -12,7 +12,7 @@ public class DecendingOrderComparator {
 		List<Integer> in=Arrays.asList(12,45,67,87,89,9);
 		Collections.sort(in);
 		System.out.println(in);
-		//Collections.sort(in, (o1, o2) -> o1>o2?-1:o1<o2?1:0 );
+		Collections.sort(in, (o1, o2) -> o1>o2?-1:o1<o2?1:0 );
 		
 		Comparator<Integer> cm=(o1, o2) -> o1>o2?-1:o1<o2?1:0;
 		Collections.sort(in,cm);
